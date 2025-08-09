09ce1e91cc045fb23877e4adb5193583d3635f13
Abdullah mohamed <112867836+mohamed12345abdullah@users.noreply.github.com>
2025-08-09 13:46:31 +0300

    push  / for the static

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+
+const path = require("path");
+app.use("/",express.static(path.join(__dirname,"public")));
 
 const orderRouter = require("./routes/ordefRouter");
 
